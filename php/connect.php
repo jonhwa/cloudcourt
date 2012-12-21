4c573df5fc1bc1640b6d9fb6655415a9a7dbe2d1
Jonathan Hwa <jonhwa@gmail.com>
2012-12-21 01:19:13 -0800

    fix session in connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,4 +1,7 @@
 <?php 
+session_start();
+ob_start();
+
 function pg_connection_string_from_database_url() {
 	extract(parse_url($_ENV["DATABASE_URL"]));
 	return "user=$user password=$pass host=$host dbname=" . substr($path, 1);
