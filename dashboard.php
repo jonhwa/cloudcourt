c0a09b6f814d0e6fa8fa660aacc015c139502e6d
Jonathan Hwa <jonhwa@gmail.com>
2012-12-22 11:43:32 -0800

    final test xml

--- dashboard.php
+++ dashboard.php
@@ -52,7 +52,7 @@
 							alert('Success: ' + data);
 							var events = [];
 							console.log(data);
-							alert('Text1: ' + $(data).find('div').attr('start') + 'Text2: ' + $(data).find('div').attr('start'));
+							alert('Text1: ' + $(data).find('div').find('start').text() + 'Text2: ' + $(data).find('div').attr('start'));
 							$(data).find('div').each(function() {
 								var event_id = $(this).attr('id');
 								alert('id: ' + event_id);
